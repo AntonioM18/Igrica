84a0e6f66073e5712a9ec6af791b2aba6857d452
AntonioM18 <134615123+AntonioM18@users.noreply.github.com>
2023-09-25 07:28:41 +0200

    Add files via upload

---
+++ game.c
@@ -0,0 +1,26 @@
+﻿// game.c
+#include "game.h"
+
+void logic(GameData* game) {
+    for (int i = 0; i < game->prepreke; i++) {
+        if (game->obstacles[i].y == 0) {
+            game->obstacles[i].y = game->visina - 1;
+            game->obstacles[i].x = rand() % game->visina;
+            (game->score)++;
+        }
+        else {
+            game->obstacles[i].y--;
+        }
+    }
+
+    if (game->score % SPEED_INCREASE_INTERVAL == 0 && game->prepreke < MAX_OBSTACLES) {
+        (game->prepreke)++;
+        (game->speed) -= 10;
+    }
+
+    // Provjera da li se score povećao za 5 i povećanje broja prepreka
+    if (game->score % 5 == 0 && game->score > 0 && game->score / 5 > game->brojPrepreka) {
+        game->brojPrepreka++;
+    }
+}
+
---
+++ game.h
@@ -0,0 +1,35 @@
+﻿// game.h
+#ifndef GAME_H
+#define GAME_H
+
+#define MAX_OBSTACLES 5
+#define INITIAL_SPEED 100
+#define SPEED_INCREASE_INTERVAL 10
+
+typedef struct {
+    int x;
+    int y;
+} Obstacle;
+
+typedef struct {
+    int visina;
+    int sirina;
+    int x;
+    int y;
+    int score;
+    int speed;
+    int prepreke;
+    int brojPrepreka;  // Novi član za praćenje broja prepreka
+    Obstacle obstacles[MAX_OBSTACLES];
+} GameData;
+
+void setup(GameData* game);
+void draw(const GameData* game);
+void input(GameData* game);
+void logic(GameData* game);
+
+#endif // GAME_H
+
+
+
+
